c5c4b00f1c44c69cf835d4308425e2993738905a
David Cohen <dcohen@hashicorp.com>
2022-07-05 18:34:25 -0400

    add the 'done' subcommand

---
+++ cmd/done.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/boltdb/bolt"
+	"github.com/spf13/cobra"
+)
+
+// doneCmd represents the done command
+var doneCmd = &cobra.Command{
+	Use:   "done",
+	Short: "lists tasks that have been done",
+	Long:  `Sub-command of 'list' -- lists only tasks that have been done.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		db := initDB()
+		defer db.Close()
+
+		err := db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("tasks"))
+			c := b.Cursor()
+
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				if string(v) == "done" {
+					fmt.Printf("DONE: %s\n", k)
+				}
+			}
+			return nil
+		})
+
+		if err != nil {
+			fmt.Println("ERROR: something went wrong while trying to view the DB: ", err)
+		}
+	},
+}
+
+func init() {
+	listCmd.AddCommand(doneCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// doneCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// doneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
